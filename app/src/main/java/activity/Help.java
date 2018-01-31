8f7a8dfd6eb78e3e6104c5f7a939c9025798a75e
Carol <carolmuchoki95@gmail.com>
2018-01-31 23:32:51 +0300

    initial commit

---
+++ app/src/main/java/activity/Help.java
@@ -0,0 +1,7 @@
+package activity;
+
+/**
+ * Created by Caro on 1/31/2018.
+ */
+public class Help {
+}
---
+++ app/src/main/java/other/CircleTransform.java
@@ -0,0 +1,7 @@
+package other;
+
+/**
+ * Created by Caro on 1/31/2018.
+ */
+public class CircleTransform {
+}
